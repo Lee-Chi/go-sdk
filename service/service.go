b4594561c7eae03aaecc58f22bf918f86181600e
leo.lee <leo.lee@becreator.com.tw>
2024-03-05 14:33:59 +0800

    changed remove log in service

--- service/service.go
+++ service/service.go
@@ -12,8 +12,6 @@ var sig chan os.Signal
 
 // Launch ready to start the service and awaiting the  signal to gracefully shut down
 func Launch() {
-	logger.Info("service is running...")
-
 	sig = make(chan os.Signal, 1)
 
 	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
