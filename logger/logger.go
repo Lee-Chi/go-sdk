d859354b5d3f9c1e69a38ca69b7c3a90c9ca7fae
leo.lee <leo.lee@becreator.com.tw>
2024-03-07 11:01:08 +0800

    changed logger

--- logger/logger.go
+++ logger/logger.go
@@ -26,10 +26,14 @@ func getCurrentDir() string {
 }
 
 const (
-	Level_Error = "ERROR"
-	Level_Warn  = "WARN"
-	Level_Info  = "INFO"
-	Level_Debug = "DEBUG"
+	Level_Error = "E"
+	Level_Info  = "I"
+	Level_Warn  = "W"
+	Level_Debug = "D"
+)
+
+const (
+	TimeLayout string = "2006-01-02 15:04:05.000"
 )
 
 func log(level string, format string, args ...interface{}) {
@@ -43,7 +47,37 @@ func log(level string, format string, args ...interface{}) {
 		file = file[len(ignoreDir)+1:]
 	}
 
-	fmt.Printf("%s [%s] %s:%d %s\n", time.Now().Format(time.RFC3339Nano), level, file, line, fmt.Sprintf(format, args...))
+	fmt.Printf("[%s] %s | %s:%d | %s\n", level, time.Now().UTC().Format(TimeLayout), file, line, fmt.Sprintf(format, args...))
+}
+
+func Printf(format string, args ...interface{}) {
+	_, file, line, ok := runtime.Caller(1)
+
+	if !ok {
+		file = "???"
+		line = 0
+	}
+
+	if ignoreDir != "" {
+		file = file[len(ignoreDir)+1:]
+	}
+
+	fmt.Printf("%s | %s:%d | %s", time.Now().UTC().Format(TimeLayout), file, line, fmt.Sprintf(format, args...))
+}
+
+func Sprintf(format string, args ...interface{}) string {
+	_, file, line, ok := runtime.Caller(1)
+
+	if !ok {
+		file = "???"
+		line = 0
+	}
+
+	if ignoreDir != "" {
+		file = file[len(ignoreDir)+1:]
+	}
+
+	return fmt.Sprintf("%s | %s:%d | %s", time.Now().UTC().Format(TimeLayout), file, line, fmt.Sprintf(format, args...))
 }
 
 func Error(format string, args ...interface{}) {
